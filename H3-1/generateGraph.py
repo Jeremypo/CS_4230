80750b73daac245d24ae7467952037697ab8bac5
Jeremy <96376044+Jeremypo@users.noreply.github.com>
2024-04-10 15:34:23 -0700

    generatedGraphs

---
+++ H3-1/generateGraph.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import random
+import time
+
+start_time = time.time()
+
+
+def generate_connected_graph(edges, seed=1):
+    max_nodes_percentage = 4.5 / 100    # same ratio of nodes to edges in twitter
+    max_nodes = max(int(edges * max_nodes_percentage), 2)
+    G = nx.generators.trees.random_tree(n=max_nodes, seed=seed)
+    while G.number_of_edges() < edges:
+        a, b = random.sample(range(max_nodes), 2)
+        if not G.has_edge(a, b):
+            G.add_edge(a, b)
+    return G
+
+
+def write_edges_to_file(graph, file_path):
+    with open(file_path, 'w') as file:
+        for edge in graph.edges():
+            file.write(f"{edge[0]} {edge[1]}\n")
+
+
+x = 176814 # 10% of twitter number of nodes
+graph = generate_connected_graph(x)
+
+print(f"Number of edges: {graph.number_of_edges()}")
+print(f"Number of nodes: {graph.number_of_nodes()}")
+
+output_file = 'graph_edges.txt'
+write_edges_to_file(graph, output_file)
+end_time = time.time()
+print(f"Edges written to {output_file}")
+print(f"Execution time: {end_time - start_time} seconds")
---
+++ H3-1/partOne.py
@@ -0,0 +1,78 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import multiprocessing
+import datetime
+
+
+def build_graph(file_path):
+    directed_graph = nx.DiGraph()
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            node1, node2 = line.strip().split()
+            directed_graph.add_edge(node1, node2)
+
+    undirected_graph = directed_graph.to_undirected()
+    return undirected_graph
+
+
+def compute_centrality_parallel(graph, centrality_func):
+    return centrality_func(graph)
+
+
+def compute_centrality(graph):
+    with multiprocessing.Pool(processes=3) as pool:
+        results = pool.starmap(compute_centrality_parallel, [
+            (graph, nx.degree_centrality),
+            (graph, nx.closeness_centrality),
+            (graph, nx.betweenness_centrality)
+        ])
+    return results
+
+
+def plot_histogram(data, title, xlabel, ylabel='Frequency'):
+    plt.figure(figsize=(10, 6))
+    plt.hist(data.values(), bins=50, alpha=0.7)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(title + '.png')
+    plt.close()
+
+
+def analyze_top_nodes(degree_cent, closeness_cent, betweenness_cent):
+    top_nodes = sorted(degree_cent, key=degree_cent.get, reverse=True)[:200]
+    closeness_values = [closeness_cent[node] for node in top_nodes]
+    betweenness_values = [betweenness_cent[node] for node in top_nodes]
+
+    closeness_stats = (np.mean(closeness_values), np.median(closeness_values), np.std(closeness_values))
+    betweenness_stats = (np.mean(betweenness_values), np.median(betweenness_values), np.std(betweenness_values))
+
+    return closeness_stats, betweenness_stats
+
+
+if __name__ == "__main__":
+
+    start_time = datetime.datetime.now()
+
+    #file_path = 'twitter_combined.txt'
+    #file_path = 'test.txt'
+    file_path = 'graph_edges.txt'
+    graph = build_graph(file_path)
+
+    degree, closeness, betweenness = compute_centrality(graph)
+
+    plot_histogram(degree, 'Degree_Centrality_Histogram', 'Degree Centrality')
+    plot_histogram(closeness, 'Closeness_Centrality_Histogram', 'Closeness Centrality')
+    plot_histogram(betweenness, 'Betweenness_Centrality_Histogram', 'Betweenness Centrality')
+
+    closeness_stats, betweenness_stats = analyze_top_nodes(degree, closeness, betweenness)
+
+    with open('centrality_analysis_results.txt', 'w') as file:
+        file.write("Closeness (Mean, Median, Std): {}\n".format(closeness_stats))
+        file.write("Betweenness (Mean, Median, Std): {}\n".format(betweenness_stats))
+
+    end_time = datetime.datetime.now()
+    execution_time = end_time - start_time
+    print(f"Time: {execution_time} (Total)")
